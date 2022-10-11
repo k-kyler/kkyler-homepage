3f9cd067c7eba88b1d4fead34fcb6fdfec3d9279
k-kyler <khaiquang690@gmail.com>
2022-10-11 22:39:16 +0700

    feat: update font family

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,13 @@ import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
 
 import { theme } from '@/config/theme';
+import { GlobalStyles } from '@/constants';
 import Layout from '@/layouts/Layout/Layout';
 
 const MyApp = ({ Component, pageProps, router }: AppProps) => {
   return (
     <ChakraProvider theme={theme}>
+      <GlobalStyles />
       <Layout router={router}>
         <Component key={router.route} {...pageProps} />
       </Layout>
