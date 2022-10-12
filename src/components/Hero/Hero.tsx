ecb19824c4eb0f2cba301894cb40fd5b5a4d05b5
k-kyler <khaiquang690@gmail.com>
2022-10-12 23:36:12 +0700

    feat: config spacing in hero section

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,9 +1,15 @@
-import { Box } from '@chakra-ui/react';
+import { Box, useColorModeValue } from '@chakra-ui/react';
 import React from 'react';
 
 const Hero = () => {
   return (
-    <Box bg="gray" p={3} mb={6} textAlign="center" borderRadius="lg">
+    <Box
+      p={3}
+      my={6}
+      textAlign="center"
+      borderRadius="lg"
+      bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+    >
       {"What's up. I'm Khai - a software engineer based in Vietnam 🍺"}
     </Box>
   );
