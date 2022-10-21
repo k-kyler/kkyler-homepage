a3a8e36aa2ae583a79e2ceb0d581a00419879ce0
k-kyler <khaiquang690@gmail.com>
2022-10-22 00:33:08 +0700

    feat: configure mobile display for self image

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -1,4 +1,4 @@
-import { Box, Heading, Image } from '@chakra-ui/react';
+import { Box, Heading, Image, Stack } from '@chakra-ui/react';
 import React from 'react';
 
 const Intro: React.FC = () => {
@@ -8,9 +8,8 @@ const Intro: React.FC = () => {
         <Heading as="h2">Khai Bui</Heading>
         <p>Software Engineer (Front-end / Back-end / Mobile)</p>
       </Box>
-      <Box
-        flexShrink={0}
-        alignSelf="center"
+      <Stack
+        alignItems="center"
         mt={{ base: 4, md: 0 }}
         ml={{ base: 0, md: 6 }}
       >
@@ -25,7 +24,7 @@ const Intro: React.FC = () => {
           borderColor="whiteAlpha.800"
           maxWidth="96px"
         />
-      </Box>
+      </Stack>
     </Box>
   );
 };
