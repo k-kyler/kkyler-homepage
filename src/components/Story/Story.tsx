9b5573ae4db03e7fecd6b13da44521df31f0c948
k-kyler <khaiquang690@gmail.com>
2023-07-27 01:38:33 +0700

    feat: update story

--- src/components/Story/Story.tsx
+++ src/components/Story/Story.tsx
@@ -22,11 +22,11 @@ const Story: React.FC = () => {
   const storyData: { paragraph: React.ReactNode }[] = [
     {
       paragraph:
-        "I'm a software engineer with a deep passion in the web development world. I've been diving into it during self-learning, university, and having hands-on experience with both front-end and back-end along with a few side projects. I love learning new technologies, new stuff, or everything that can help me build great products.",
+        "Hi! I'm a developer with a deep passion in web development world, I've been diving into it during self-learning, university, work and having hands-on experience with both front-end and back-end along with a few side projects. I love learning new technologies, new stuff or everything that can help me make great web apps.",
     },
     {
       paragraph:
-        "My main interest in software development is about making attractive user interfaces and giving end-users great experiences, so I mostly like doing the front-end. However, I'm also very interested in doing the back-end, mobile, or any aspects of the software development process.",
+        "My big interest in web development is about making attractive user interface and giving end-user awesome experience, so I mostly like doing the front-end. However, I'm also willing to learn more and taking part in different aspects of the software development.",
     },
     {
       paragraph: (
